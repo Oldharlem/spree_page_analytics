7b4bd31dac617ed80e5b4e8b9abe64ce9d9d79ef
Kyle Van Wagenen <kvanwagenen@gmail.com>
2015-07-30 18:43:29 -0600

    Automatically create snapshots for at least the last four months

--- app/models/spree/analytics/google_analytics_service.rb
+++ app/models/spree/analytics/google_analytics_service.rb
@@ -15,31 +15,42 @@ module Spree
         )
       end
 
-      def create_snapshots(date=nil)
-        
-        # Request data from Google
+      def create_snapshots(start_date=nil, end_date=nil)
         if !@client.authorized?
           @client.authorize
         end
-        yesterday = Time.now - 1.day
-        date ||= yesterday
-        start_date ||= date.beginning_of_day
-        end_date ||= date.end_of_day
+        start_date ||= begin
+          newest_snapshot = Spree::PageTrafficSnapshot.order("begin DESC").first
+          newest_snapshot_start = newest_snapshot ? newest_snapshot.begin.beginning_of_day + 1.day : nil
+          four_months_ago = Time.now.beginning_of_day - 4.months
+          if newest_snapshot_start
+            [newest_snapshot_start, four_months_ago].max
+          else
+            four_months_ago
+          end
+        end
+        end_date ||= Time.now.end_of_day - 1.day
+        date = start_date
+        while date < end_date
+          create_snapshots_for_date(date)
+          date += 1.day
+        end
+      end
+
+      def create_snapshots_for_date(date)
         dimensions = "ga:landingPagePath"
         metrics = "ga:sessions,ga:transactions,ga:transactionRevenue"
         sort = "ga:landingPagePath"
         filters = "ga:landingPagePath=@/t/,ga:landingPagePath=@/products/,ga:landingPagePath==/"
-        result = @client.get_data(start_date, end_date, dimensions, metrics, filters, sort)
-
-        # Create snapshot for each row
+        result = @client.get_data(date.beginning_of_day, date.end_of_day, dimensions, metrics, filters, sort)
         result[:rows].each do |row|
           Spree::PageTrafficSnapshot.create(
             :page => row[0], 
             :sessions => row[1], 
             :transactions => row[2],
             :revenue => row[3],
-            :begin => start_date,
-            :end => end_date
+            :begin => date.beginning_of_day,
+            :end => date.end_of_day
           )
         end
       end
